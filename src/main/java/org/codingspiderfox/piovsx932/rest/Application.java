fb0419745d92b56ba977c59ea49194c761eacf1f
CodingSpiderFox <sunrisechain@gmail.com>
2018-08-28 14:45:37 +0200

    Import Spring boot project

---
+++ src/main/java/org/codingspiderfox/piovsx932/rest/Application.java
@@ -0,0 +1,12 @@
+package org.codingspiderfox.piovsx932.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
